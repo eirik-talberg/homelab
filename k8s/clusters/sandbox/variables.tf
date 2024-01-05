6a7330b00f325d2c688302ea9b481931328d1507
Eirik W. Talberg <eirik@taldev.no>
2024-01-05 16:25:35 +0100

    Fix sandbox k8s cluster setup.

--- k8s/clusters/sandbox/variables.tf
+++ k8s/clusters/sandbox/variables.tf
@@ -2,39 +2,27 @@ locals {
   domain         = "taldev.no"
   subnet         = "10.0.69"
   inventory_name = "sandbox"
+  pve_hostname   = "pve-sbx"
+  target_node    = "pve-sbx-01"
   nodes          = {
     "master" = {
       "k3s-sbx-01" = {
-        vm_id   = 151
-        cores   = 2
-        memory  = 2048
-        os_disk = {}
+        vm_id    = 151
+        template = "ubuntu-2204-medium"
       },
     },
     "agent" = {
       "k3s-sbx-02" = {
-        vm_id   = 152
-        cores   = 2
-        memory  = 4096
-        os_disk = {
-          size = "32G"
-        }
+        vm_id    = 152
+        template = "ubuntu-2204-medium"
       },
       "k3s-sbx-03" = {
-        vm_id   = 153
-        cores   = 2
-        memory  = 4096
-        os_disk = {
-          size = "32G"
-        }
+        vm_id    = 153
+        template = "ubuntu-2204-medium"
       },
       "k3s-sbx-04" = {
-        vm_id   = 154
-        cores   = 2
-        memory  = 4096
-        os_disk = {
-          size = "32G"
-        }
+        vm_id    = 154
+        template = "ubuntu-2204-medium"
       }
     }
   }
