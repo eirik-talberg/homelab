84571a1a11a9d761c1cd27acef04a21656242be9
Eirik W. Talberg <eirik@taldev.no>
2024-01-05 16:25:46 +0100

    Add complete operations k8s cluster setup.

--- k8s/clusters/operations/main.tf
+++ k8s/clusters/operations/main.tf
@@ -3,23 +3,32 @@ locals {
 }
 
 module "master-nodes" {
-  source = "../../../tfmodules/pve-vms"
-  nodes  = local.nodes.master
-  subnet = local.subnet
-  domain = local.domain
+  source       = "../../../tfmodules/pve-vms"
+  nodes        = local.nodes.master
+  subnet       = local.subnet
+  domain       = local.domain
+  pve_hostname = local.pve_hostname
+  target_node  = local.target_node
 }
 
 module "agent-nodes" {
-  source = "../../../tfmodules/pve-vms"
-  nodes  = local.nodes.agent
-  subnet = local.subnet
-  domain = local.domain
+  source       = "../../../tfmodules/pve-vms"
+  nodes        = local.nodes.agent
+  subnet       = local.subnet
+  domain       = local.domain
+  pve_hostname = local.pve_hostname
+  target_node  = local.target_node
 }
-
 resource "local_file" "ansible_inventory_file" {
   content = templatefile("${local.project_root}/ansible/inventories/k3s_hosts.tftpl", {
-    master_nodes = [for hostname, values in local.nodes.master : "${hostname}.${local.domain} ansible_host=${local.subnet}.${values.vm_id}"]
-    agent_nodes  = [for hostname, values in local.nodes.agent : "${hostname}.${local.domain} ansible_host=${local.subnet}.${values.vm_id}"]
+    master_nodes = [
+      for hostname, values in local.nodes.master :
+      "${hostname}.${local.domain} ansible_host=${local.subnet}.${values.vm_id}"
+    ]
+    agent_nodes  = [
+      for hostname, values in local.nodes.agent :
+      "${hostname}.${local.domain} ansible_host=${local.subnet}.${values.vm_id}"
+    ]
   })
   filename = "${local.project_root}/ansible/inventories/${local.inventory_name}/hosts.ini"
 }
--- k8s/clusters/operations/variables.tf
+++ k8s/clusters/operations/variables.tf
@@ -2,18 +2,37 @@ locals {
   domain         = "taldev.no"
   subnet         = "10.0.69"
   inventory_name = "operations"
+  pve_hostname   = "pve"
+  target_node    = "pve-prod-01"
   nodes          = {
     "master" = {
       "k3s-ops-01" = {
-        vm_id   = 161
-        cores   = 8
-        memory  = 8192
-        os_disk = {
-            size = "32G"
-        }
+        vm_id    = 161
+        template = "ubuntu-2204-medium"
       },
+      "k3s-ops-02" = {
+        vm_id    = 162
+        template = "ubuntu-2204-medium"
+      },
+      "k3s-ops-03" = {
+        vm_id    = 163
+        template = "ubuntu-2204-medium"
+      }
     },
     "agent" = {
+      "k3s-ops-04" = {
+        vm_id    = 164
+        template = "ubuntu-2204-large"
+      },
+      "k3s-ops-05" = {
+        vm_id    = 165
+        template = "ubuntu-2204-large"
+
+      },
+      "k3s-ops-06" = {
+        vm_id    = 166
+        template = "ubuntu-2204-large"
+      }
     }
   }
 }
